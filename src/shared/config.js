348bd7eabf80ee1373fba87c02e335b8a1a436b3
pengyang <pengyang@iynxiao.com>
2018-02-13 22:24:13 +0800

    finally yohoo!

---
+++ src/shared/config.js
@@ -0,0 +1,8 @@
+// @flow
+
+export const WEB_PORT = process.env.PORT || 8000
+export const STATIC_PATH = '/static'
+export const APP_NAME = 'Hello JS_STACK_FROM_SCRATCH'
+export const WDS_PORT = 7000
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
